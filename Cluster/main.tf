581753f0ee535929da239258122e5aa7ece991fe
gowdas1997 <154775960+gowdas1997@users.noreply.github.com>
2025-01-04 17:41:29 +0530

    Update main.tf

--- Cluster/main.tf
+++ Cluster/main.tf
@@ -99,7 +99,7 @@ resource "aws_eks_cluster" "devopsshack" {
 resource "aws_eks_node_group" "devopsshack" {
   cluster_name    = aws_eks_cluster.devopsshack.name
   node_group_name = "devopsshack-node-group"
-  node_role_arn   = aws_iam_role.devopsshack_node_group_role.arn
+  node_role_arn   = aws_iam_role.devopsshack_node_group_role1.arn
   subnet_ids      = aws_subnet.devopsshack_subnet[*].id
 
   scaling_config {
